19aff9e9b3321c901b7a8c9c8f8f574f5dcdb98a
MikhailShokun <minishokun@gmail.com>
2021-12-21 11:01:05 +0200

    added final elements

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ const addToShopping = document.querySelectorAll(".pizza__addToShopping");
 
 for (let i of addToShopping) {
     i.addEventListener("click", function() {
-        i.querySelector('img').src = 'img/ellipse_white.png';
+        i.querySelector('img').src = 'img/WhiteBag_OrangeBackground.png';
     });
 }
